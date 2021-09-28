0660e6aca51ac6120101b8592349cbb1e00e9518
Cesar0158 <90809384+Cesar0158@users.noreply.github.com>
2021-09-28 10:11:26 -0500

    Add files via upload

--- TrabajoWeb/eatSafe/src/main/java/pe/edu/upc/eatSafe/controller/RestaurantView.java
+++ TrabajoWeb/eatSafe/src/main/java/pe/edu/upc/eatSafe/controller/RestaurantView.java
@@ -16,17 +16,17 @@ import pe.edu.upc.eatSafe.model.entity.Restaurant;
 
 @Named("restaurantView")
 @ViewScoped
-public class RestaurantView implements Serializable{
-	
-private static final long serialVersionUID = 1L;
-	
+public class RestaurantView implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	private List<Restaurant> restaurants;
 	private Restaurant restaurantSelected;
-	private List<Restaurant> restaurantsSelected;	
-	
+	private List<Restaurant> restaurantsSelected;
+
 	@Inject
 	private RestaurantService restaurantService;
-	
+
 	@PostConstruct
 	public void init() {
 		restaurantsSelected = new ArrayList<>();
@@ -36,46 +36,65 @@ private static final long serialVersionUID = 1L;
 			e.printStackTrace();
 		}
 	}
-	
+
 	public boolean hasRestaurantsSelected() {
 		if (restaurantsSelected.isEmpty()) {
 			return false;
 		}
 		return true;
 	}
+
 	public boolean hasRestaurantSelected() {
 		if (restaurantsSelected.size() == 1) {
 			return true;
 		}
 		return false;
 	}
+
 	public void createNew() {
-		restaurantSelected = new Restaurant();		
+		restaurantSelected = new Restaurant();
 	}
+
 	public void editRestaurantSelected() {
 		restaurantSelected = restaurantsSelected.get(0);
 	}
+
 	public void saveRestaurant() {
 		try {
 			System.out.println(restaurantSelected.getId());
 			System.out.println(restaurantSelected.getName());
-			
+			System.out.println(restaurantSelected.getAddress());
+			System.out.println(restaurantSelected.getCapacity());
+			System.out.println(restaurantSelected.getPhone());
+			System.out.println(restaurantSelected.getOpinions());
+			System.out.println(restaurantSelected.getTables());
+
 			if (restaurantSelected.getId() == null) {
 				restaurantService.create(restaurantSelected);
 				restaurants.add(restaurantSelected);
-			} 
-			else {
+			} else {
 				restaurantService.update(restaurantSelected);
-			}			
-			
+			}
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		PrimeFaces.current().executeScript("PF('restaurantDialog').hide()");
-        PrimeFaces.current().ajax().update("restaurantDataTable");
+		PrimeFaces.current().ajax().update("restaurantDataTable");
 	}
-	
+
+	public void deleteRestaurant() {
+		try {
+			if (restaurantSelected.getId() > 0) {
+				restaurantService.deleteById(restaurantSelected.getId());
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public List<Restaurant> getRestaurants() {
 		return restaurants;
 	}
