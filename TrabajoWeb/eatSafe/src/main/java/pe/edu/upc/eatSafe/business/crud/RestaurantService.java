f0387a3fa43587e65dbad1cb22a50f88c7177fc8
Cesar0158 <90809384+Cesar0158@users.noreply.github.com>
2021-09-28 20:55:00 -0500

    Add files via upload

--- TrabajoWeb/eatSafe/src/main/java/pe/edu/upc/eatSafe/business/crud/RestaurantService.java
+++ TrabajoWeb/eatSafe/src/main/java/pe/edu/upc/eatSafe/business/crud/RestaurantService.java
@@ -1,7 +1,9 @@
 package pe.edu.upc.eatSafe.business.crud;
 
+import java.util.List;
+
 import pe.edu.upc.eatSafe.model.entity.Restaurant;
 
 public interface RestaurantService extends CrudService<Restaurant, Integer> {
-
+List<Restaurant> findByName(String name) throws Exception;
 }
