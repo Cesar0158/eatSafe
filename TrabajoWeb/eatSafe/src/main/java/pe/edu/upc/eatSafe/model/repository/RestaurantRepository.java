3f6515e13eb98cd872caee3928f3a1b4ca0c1437
Cesar0158 <90809384+Cesar0158@users.noreply.github.com>
2021-09-28 20:56:06 -0500

    Add files via upload

--- TrabajoWeb/eatSafe/src/main/java/pe/edu/upc/eatSafe/model/repository/RestaurantRepository.java
+++ TrabajoWeb/eatSafe/src/main/java/pe/edu/upc/eatSafe/model/repository/RestaurantRepository.java
@@ -1,7 +1,10 @@
 package pe.edu.upc.eatSafe.model.repository;
 
+import java.util.List;
+
 import pe.edu.upc.eatSafe.model.entity.Restaurant;
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
 
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+	List<Restaurant> findByName(String name) throws Exception;
 }
