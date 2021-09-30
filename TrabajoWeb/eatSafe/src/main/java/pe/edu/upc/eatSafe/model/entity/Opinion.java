69e8e91e2f1cc14f746fbfd0cbddd032bbc11e60
Cesar0158 <90809384+Cesar0158@users.noreply.github.com>
2021-09-29 22:43:12 -0500

    Update Opinion.java

--- TrabajoWeb/eatSafe/src/main/java/pe/edu/upc/eatSafe/model/entity/Opinion.java
+++ TrabajoWeb/eatSafe/src/main/java/pe/edu/upc/eatSafe/model/entity/Opinion.java
@@ -10,65 +10,75 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Opinions")
+@Table(name = "Opinions")
 public class Opinion {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "opinion_id")
-private Integer id;
-	@Column(name = "opinion_qualification",length = 2,nullable=false)
-private Integer qualification;
+	private Integer id;
+	@Column(name = "opinion_qualification", length = 2, nullable = false)
+	private Integer qualification;
 	@Column(name = "opinion_comment", length = 60)
-private String comment;
-@ManyToOne
-@JoinColumn(name = "client_id",nullable = false)//Foreing Key
-private Client client;
-@ManyToOne
-@JoinColumn(name = "restaurant_id",nullable = false)//Foreing Key
-private Restaurant restaurant;
-public Opinion(Integer id, Integer qualification, String comment, Client client, Restaurant restaurant) {
-	super();
-	this.id = id;
-	this.qualification = qualification;
-	this.comment = comment;
-	this.client = client;
-	this.restaurant = restaurant;
-}
+	private String comment;
+	@ManyToOne
+	@JoinColumn(name = "client_id", nullable = false) // Foreing Key
+	private Client client;
+	@ManyToOne
+	@JoinColumn(name = "restaurant_id", nullable = false) // Foreing Key
+	private Restaurant restaurant;
 
-public Opinion() {
-	super();
-	// TODO Auto-generated constructor stub
-}
+	public Opinion(Integer id, Integer qualification, String comment, Client client, Restaurant restaurant) {
+		super();
+		this.id = id;
+		this.qualification = qualification;
+		this.comment = comment;
+		this.client = client;
+		this.restaurant = restaurant;
+	}
 
-public Integer getId() {
-	return id;
-}
-public void setId(Integer id) {
-	this.id = id;
-}
-public Integer getQualification() {
-	return qualification;
-}
-public void setQualification(Integer qualification) {
-	this.qualification = qualification;
-}
-public String getComment() {
-	return comment;
-}
-public void setComment(String comment) {
-	this.comment = comment;
-}
-public Client getClient() {
-	return client;
-}
-public void setClient(Client client) {
-	this.client = client;
-}
-public Restaurant getRestaurant() {
-	return restaurant;
-}
-public void setRestaurant(Restaurant restaurant) {
-	this.restaurant = restaurant;
-}
+	public Opinion() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getQualification() {
+		return qualification;
+	}
+
+	public void setQualification(Integer qualification) {
+		this.qualification = qualification;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Restaurant getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
 
 }
